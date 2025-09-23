2db728d6295155e55ad4926e563aa61896a15820
liuzhizhen <zhizhen.liu@publicissapient.com>
2025-09-23 18:30:03 +0800

    修改productType

--- libs/productType.ts
+++ libs/productType.ts
@@ -1,21 +1,40 @@
-import productTypes from "../data/productTypes.json";
-import fs from "fs";
+import {readFile,writeFile} from "fs/promises";
+const filePath = "../data/productTypes.json"
 interface ProductType{
-    name:String,
-    unit:Array<String>,
-    pic:String,
+    id:number,
+    name:string,
+    unit:Array<string>,
+    pic:string,
     description:String
 }
-export function getProductTypes():Array<ProductType>{   
-    return productTypes;
-}
-export function getUnitByProductType(productType:string):Array<String>{
-    const product=productTypes.find((item:ProductType)=>item.name===productType);
-    if(product){
-        return product.unit;
+type QueryProductType = Partial<Pick<ProductType, 'id' | 'name'>>;
+//查询产品类型
+export async function getProductTypes(query:QueryProductType):Promise<ProductType[]>{
+    try {
+        const allProductTypes:Array<ProductType> = await readFile(filePath)
+        return allProductTypes.filter(item=>{
+            const matchesId = query.id === undefined || query.id === item.id;
+            const matchesName = query.name === undefined || query.name === item.name;
+            return matchesId && matchesName;
+        });
+    } catch (error) {
+        console.error(error)
+        return []
     }
-    return [];
 }
+//修改产品类型
+export async function editProductType(newProductType:ProductType):Promise<void>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+//删除产品类型
+export function deleteProductType({id:number}){
+    writeFile(filePath,JSON.stringify([...productTypes,productType],null,2));
+}
+//新增产品类型
 export function addProductType(productType:ProductType):void{
-    fs.writeFileSync("../data/productTypes.json",JSON.stringify([...productTypes,productType],null,2));
+    writeFile(filePath,JSON.stringify([...productTypes,productType],null,2));
 }
